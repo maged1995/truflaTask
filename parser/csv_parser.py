b02cdc71676579a42c11815d2cad7f2ff62263b7
Maged <maged@thewebop.com>
2021-09-27 14:19:56 +0200

    handling exceptions

--- parser/csv_parser.py
+++ parser/csv_parser.py
@@ -12,16 +12,23 @@ class CSVParser(ParserMain):
     def enrich_data(self, row):
         vin = row["vin_number"]
         model_year = row["model_year"]
-        response = requests.get(f"https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValues/{vin}?format=json&modelyear={model_year}")
-        
-        if response.status_code == 200:
-            extra_info = response.json()['Results'][0]
-            self.vehicle_data.update({
-                "model": extra_info['Model'],
-                "manufacturer": extra_info['Manufacturer'],
-                "plant_country": extra_info['PlantCountry'],
-                "vehicle_type": extra_info['VehicleType'] 
-            }) 
+        try:
+            response = requests.get(f"https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValues/{vin}?format=json&modelyear={model_year}")
+            
+            if response.status_code == 200:
+                extra_info = response.json()['Results'][0]
+                self.vehicle_data.update({
+                    "model": extra_info['Model'],
+                    "manufacturer": extra_info['Manufacturer'],
+                    "plant_country": extra_info['PlantCountry'],
+                    "vehicle_type": extra_info['VehicleType'] 
+                })
+            else:
+                self.exceptions.add({"vehicle_id": row["id"], "exception": "wrong or unavailable vehicle data"})
+        except requests.exceptions.ConnectionError:
+            self.exceptions.add('No Internet Connection for Data enrichment')
+        except:
+            self.exceptions.add('Unknown Error Occurred. Contact the developer')
 
     def pre_process(self):
         data1 = pd.read_csv(self.file_name)
@@ -70,6 +77,7 @@ class CSVParser(ParserMain):
                     ]
                 })
         self.json_data = json.dumps(json_res, indent=3, ensure_ascii=False)
+        if self.exceptions: print(self.exceptions)
         return json_res
 
     def parse_to_json(self):
--- parser/xml_parser.py
+++ parser/xml_parser.py
@@ -10,16 +10,23 @@ class XMLParser(ParserMain):
         self.customer_data
         vin = self.customer_data["Units"]["Auto"]["Vehicle"][0]["VinNumber"]
         model_year = self.customer_data["Units"]["Auto"]["Vehicle"][0]["ModelYear"]
-        response = requests.get(f"https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValues/{vin}?format=json&modelyear={model_year}")
-        extra_info = response.json()['Results'][0]
 
-        if response.status_code == 200:
-            self.customer_data["Units"]["Auto"]["Vehicle"][0].update({
-                "model": extra_info['Model'],
-                "manufacturer": extra_info['Manufacturer'],
-                "plant_country": extra_info['PlantCountry'],
-                "vehicle_type": extra_info['VehicleType']
-            })
+        try:
+            response = requests.get(f"https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValues/{vin}?format=json&modelyear={model_year}")
+            extra_info = response.json()['Results'][0]
+            if response.status_code == 200:
+                self.customer_data["Units"]["Auto"]["Vehicle"][0].update({
+                    "model": extra_info['Model'],
+                    "manufacturer": extra_info['Manufacturer'],
+                    "plant_country": extra_info['PlantCountry'],
+                    "vehicle_type": extra_info['VehicleType']
+                })
+            else:
+                self.exceptions.append("wrong or unavailable vehicle data")
+        except requests.exceptions.ConnectionError:
+            self.exceptions.add('No Internet Connection for Data enrichment')
+        except:
+            self.exceptions.add('Unknown Error Occurred. Contact the developer')
 
     def pre_process(self):
         with open(self.file_name, mode='r') as xml_file:
@@ -41,6 +48,7 @@ class XMLParser(ParserMain):
                 ]
             }
             self.json_data = json.dumps(json_res, indent=3, ensure_ascii=False).replace("@", "")
+            if self.exceptions: print(self.exceptions)
             return json_res
 
     def parse_to_json(self):
